3762d60c2bd517ac295c9f95636080a19b0104ed
tinh1408 <tinhph@hotmail.com>
2018-10-19 16:03:52 +0700

    commit 19/10

--- src/com/khai/Advance.java
+++ src/com/khai/Advance.java
@@ -8,7 +8,7 @@ public class Advance {
 
         System.out.println(makeArrayConsecutive2(arr));
         System.out.println("-----------------------------------------------");
-        int[] arr1 = {3,6,-2,-5,7,3};
+        int[] arr1 = {3, 6, -2, -5, 7, 3};
         System.out.println(adjacentElementsProduct(arr1));
         System.out.println("-----------------------------------------------");
 
@@ -16,32 +16,34 @@ public class Advance {
         System.out.println(firstDuplicate(arr2));
         System.out.println("------------------------------------------------");
     }
-    static int adjacentElementsProduct(int[] arr){
+
+    static int adjacentElementsProduct(int[] arr) {
         int a = 0;
-        for(int i = 1; i < arr.length - 1; i++){
-            a = arr[i]*arr[i-1] > arr[i]*arr[i+1]?arr[i]*arr[i-1]:arr[i]*arr[i+1];
+        for (int i = 1; i < arr.length - 1; i++) {
+            a = arr[i] * arr[i - 1] > arr[i] * arr[i + 1] ? arr[i] * arr[i - 1] : arr[i] * arr[i + 1];
         }
         return a;
     }
-    static int makeArrayConsecutive2(int[] arr){
+
+    static int makeArrayConsecutive2(int[] arr) {
         Arrays.sort(arr);
         int count = 0;
-        for (int i = 1; i < arr.length; i++){
-            if(arr[i] - arr[i - 1] == 1) continue;
-            else count += arr[i] - arr[i -1] - 1 ;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == 1) continue;
+            else count += arr[i] - arr[i - 1] - 1;
         }
         return count;
     }
 
-   public static int firstDuplicate(int[] arr){
+    public static int firstDuplicate(int[] arr) {
         int a = 0, b = arr.length;
 
-        for (int i = 0; i < arr.length -1; i++){
-            for (int j = i +1; j < arr.length; j++){
-                if (arr[i]==arr[j]){
-                    if(b > j -i){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    if (b > j - i) {
                         a = arr[i];
-                        b = j -i;
+                        b = j - i;
                     }
                 }
             }
