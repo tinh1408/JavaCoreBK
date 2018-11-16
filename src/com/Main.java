8bb6d190f10a56d05499f4e82e03e16a7113b971
tinh1408 <tinhph@hotmail.com>
2018-11-16 12:08:56 +0700

    projects

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,6 @@ package com;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
-import java.util.Arrays;
 import java.util.Scanner;
 //public class Main{
 //
@@ -24,35 +23,43 @@ public class Main {
     static final String TIME_OPEN = "9PM";
 
     public static void main(String[] args) {
-//        int[] x = {3,5,6,9};
-//        int a = 0, b = 0;
-//        for(int y : x){
-//            if(y % 2 != 0)
-//            {
-//                a+=y;
-//                b++;
-//            }
-//        }
-//        System.out.println(a);
-//        System.out.println(b);
+        int a = (int)Math.round(12 * Math.random()) + 5;
+        int[] arr = {1,2,3};
+        System.out.println(m(arr));
+        System.out.println(m(1,2,3));
+//        System.out.println(m(4.1,5.2,6.3));
+        System.out.println(m());
+        System.out.println("m1"+m1(1,2,3));
+        System.out.println(a);
+        System.out.println("Math.random()"+Math.random());
+        int a4[];
+        int[] a1 = {1,2,3};
+       int[] a2= new int[]{1,2,3};
+//         int[] a3 = new int;
+        int[][] a9 = {{5, 6, 7}, {8, 9}};
 
-//        String[] a = {"Jonh", "Mary", "Caly", "Tomy"};
-//        Arrays.sort(a);
-//        System.out.println(Arrays.toString(a));
-
-//        int a[] = {4, 3, 5};
-//        a[2] = a[1] * 4;
-//        a[1] *= a.length;
-//
-//        System.out.println(Arrays.toString(a));
-        int[] x = {5,6,7};
-        for(int a : x){
-            System.out.print(a);
-        }
-
-
+        System.out.printf("--%d, %d",a9.length,a9[1].length);
 
 
     }
+
+
+    static int m(int...x) {
+        int b = 0;
+        for(int a:x){
+            b+=a;
+        }
+        return b;
+    }
+
+    static int m1(int...x) {
+        int b = 0;
+        for(int a:x){
+            b+=a;
+        }
+        return b;
+    }
+
 }
+
 
---
+++ src/com/ateach/quizz/reviewPf1/Avoid.java
@@ -0,0 +1,21 @@
+package com.ateach.quizz.reviewPf1;
+
+public class Avoid {
+    int avoidObstacles(int[] inputArray) {
+
+        int jump = 1;
+        boolean fail = true;
+
+        while(fail) {
+            jump++;
+            fail = false;
+            for(int i=0; i<inputArray.length; i++)
+                if(inputArray[i]%jump==0) {
+                    fail = true;
+                    break;
+                }
+        }
+
+        return jump;
+    }
+}
