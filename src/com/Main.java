2b412b76ac980621a3aada2cf52f6f1e8014beea
tinhph <tinhph@hotmail.c>
2018-11-18 14:40:25 +0700

    commit from

--- src/com/Main.java
+++ src/com/Main.java
@@ -24,22 +24,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a = (int)Math.round(12 * Math.random()) + 5;
-        int[] arr = {1,2,3};
-        System.out.println(m(arr));
-        System.out.println(m(1,2,3));
-//        System.out.println(m(4.1,5.2,6.3));
-        System.out.println(m());
-        System.out.println("m1"+m1(1,2,3));
         System.out.println(a);
-        System.out.println("Math.random()"+Math.random());
-        int a4[];
-        int[] a1 = {1,2,3};
-       int[] a2= new int[]{1,2,3};
-//         int[] a3 = new int;
-        int[][] a9 = {{5, 6, 7}, {8, 9}};
-
-        System.out.printf("--%d, %d",a9.length,a9[1].length);
-
 
     }
 
