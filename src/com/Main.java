2207f8829357edfc579bb2995f411f2f12047b33
tinhph <tinhph@hotmail.c>
2018-11-18 23:10:42 +0700

    updateCode

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,9 @@ package com;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
+import java.util.Arrays;
 import java.util.Scanner;
+
 //public class Main{
 //
 //    public static void main(String[] args){
@@ -23,24 +25,29 @@ public class Main {
     static final String TIME_OPEN = "9PM";
 
     public static void main(String[] args) {
-        int a = (int)Math.round(12 * Math.random()) + 5;
-        System.out.println(a);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr2;
+        int[] arr3;
+        arr3 = Arrays.copyOf(arr, 4);
+//        System.out.println(Arrays.toString(arr3));
+//      arr2 = Arrays.copyOfRange(arr,2,4);
 
+//        System.out.println(Arrays.toString(arr2));
     }
 
 
-    static int m(int...x) {
+    static int m(int... x) {
         int b = 0;
-        for(int a:x){
-            b+=a;
+        for (int a : x) {
+            b += a;
         }
         return b;
     }
 
-    static int m1(int...x) {
+    static int m1(int... x) {
         int b = 0;
-        for(int a:x){
-            b+=a;
+        for (int a : x) {
+            b += a;
         }
         return b;
     }
