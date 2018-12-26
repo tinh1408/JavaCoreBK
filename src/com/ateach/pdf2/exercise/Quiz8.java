f193c88ddc8d897dd6909aebd5b44932f4d5a99c
TINHPHAM <tinhph@hotmail.com>
2018-12-26 15:55:47 +0700

    abstract vs interface

---
+++ src/com/ateach/pdf2/exercise/Quiz8.java
@@ -0,0 +1,12 @@
+package com.ateach.pdf2.exercise;
+
+class Animal {
+}
+
+class Dog1 extends Animal {
+
+    public static void main(String args[]) {
+        Dog1 d = new Dog1();
+        System.out.println(d instanceof Animal);//true
+    }
+}
