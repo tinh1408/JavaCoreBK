6e3e29de3e27b21ba29a854ec66d8cbb24be4d34
TINHPHAM <tinhph@hotmail.com>
2019-01-21 16:40:19 +0700

    map

--- src/com/ateach/pdf2/fileInputStream/InputAndOutput.java
+++ src/com/ateach/pdf2/fileInputStream/InputAndOutput.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class InputAndOutput {
     public static void main(String[] args) {
 
-        File inFile = new File("input.txt");
-        File outFile = new File("output.txt");
+        File inFile = new File("E:\\testfile\\file.mp3");
+        File outFile = new File("E:\\testfile\\out.mp3");
 
         try {
             FileInputStream fileInputStream = new FileInputStream(inFile);
@@ -21,6 +22,20 @@ public class InputAndOutput {
             fileInputStream.close();
             fileOutputStream.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        readContentFromFile();
+    }
+
+    public static void readContentFromFile() {
+        String pathName = "E:\\testfile\\out.mp3";
+        File fl = new File(pathName);
+        try {
+            Scanner sc = new Scanner(fl);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
